5abbf8eac11c913c3e91a269ecca140f152dd8a5
Adam Hermansson <qurridutt@gmail.com>
2022-09-22 14:53:13 +0200

    first ver

---
+++ components/Face/index.jsx
@@ -0,0 +1,66 @@
+import React, { useRef, useEffect } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
+export default function Face(props) {
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF("/adam_model.glb");
+  const { actions } = useAnimations(animations, group);
+
+
+  useEffect(() => {
+    actions.idle_eyes?.play();
+  }, [actions]);
+
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <primitive object={nodes.Hips} />
+      <skinnedMesh
+        geometry={nodes.EyeLeft.geometry}
+        material={nodes.EyeLeft.material}
+        skeleton={nodes.EyeLeft.skeleton}
+      />
+      <skinnedMesh
+        geometry={nodes.EyeRight.geometry}
+        material={nodes.EyeRight.material}
+        skeleton={nodes.EyeRight.skeleton}
+      />
+      <skinnedMesh
+        name="Wolf3D_Beard"
+        geometry={nodes.Wolf3D_Beard.geometry}
+        material={materials.Wolf3D_Beard}
+        skeleton={nodes.Wolf3D_Beard.skeleton}
+        morphTargetDictionary={nodes.Wolf3D_Beard.morphTargetDictionary}
+        morphTargetInfluences={nodes.Wolf3D_Beard.morphTargetInfluences}
+      />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Hair.geometry}
+        material={materials.Wolf3D_Hair}
+        skeleton={nodes.Wolf3D_Hair.skeleton}
+      />
+      <skinnedMesh
+        name="Wolf3D_Head"
+        geometry={nodes.Wolf3D_Head.geometry}
+        material={nodes.Wolf3D_Head.material}
+        skeleton={nodes.Wolf3D_Head.skeleton}
+        morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+        morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+      />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Shirt.geometry}
+        material={materials.Wolf3D_Shirt}
+        skeleton={nodes.Wolf3D_Shirt.skeleton}
+      />
+      <skinnedMesh
+        name="Wolf3D_Teeth"
+        geometry={nodes.Wolf3D_Teeth.geometry}
+        material={materials.Wolf3D_Teeth}
+        skeleton={nodes.Wolf3D_Teeth.skeleton}
+        morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+        morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/adam_model.glb");
---
+++ components/Header/index.js
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export const Header = () => {
+  const HamburgerIcon = (
+    <button
+      type="button"
+      className="text-white bg-zinc-800 hover:bg-zinc-800 focus:ring-4 focus:outline-none focus:ring-zinc-300 rounded-full p-4 inline-flex mr-2  dark:hover:bg-zinc-700 dark:focus:ring-zinc-800 self-center lg:hidden"
+    >
+      <svg
+        aria-hidden="true"
+        className="w-4 h-4"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.5 7.5H14.5M1.5 2.5H14.5M1.5 12.5H14.5"
+          stroke="white"
+          stroke-linecap="round"
+        ></path>
+      </svg>
+      <span className="sr-only">Icon description</span>
+    </button>
+  );
+  return (
+    <header className="fixed w-screen flex justify-center top-3 h-16 ">
+      <div className="rounded-full bg-zinc-900/95 w-11/12 flex justify-between backdrop-blur-sm ">
+        <Link href="/">
+          <a>
+            <Image
+              alt="Adam face"
+              src="/logo192.png"
+              width={64}
+              height={64}
+              className="hover:cursor-pointer"
+            />
+          </a>
+        </Link>
+        {HamburgerIcon}
+      </div>
+    </header>
+  );
+};
