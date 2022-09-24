241f0bfa21f0769380b6847438d8b47d0f58ac65
kamelpaj <qurridutt@gmail.com>
2022-09-24 23:11:46 +0200

    cleanup

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -8,11 +8,11 @@ const variants = {
 
 const Layout = ({ children }) => (
   <motion.main
-    variants={variants} // Pass the variant object into Framer Motion
-    initial="hidden" // Set the initial state to variants.hidden
-    animate="enter" // Animated state to variants.enter
-    exit="exit" // Exit state (used later) to variants.exit
-    transition={{ type: "linear" }} // Set the transition to linear
+    variants={variants}
+    initial="hidden"
+    animate="enter"
+    exit="exit"
+    transition={{ type: "linear" }}
     className="pt-24 flex flex-col w-screen gap-4"
   >
     {children}
